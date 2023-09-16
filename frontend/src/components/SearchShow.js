c13c8bb9d2ee83dff479279b1cdc64a7925a1fdb
john kvassay <johnkvassay@gmail.com>
2023-09-16 16:12:45 -0700

    Began working on a structure for the frontend search functionality

---
+++ frontend/src/components/SearchShow.js
@@ -0,0 +1,10 @@
+// Element that will display all the different card series and cards within that series
+// Will also display results of searching for a specific card/cards
+
+// Imports
+import CardSm from "./CardSm";
+const ShowSearch = () => {
+    
+
+}
+export default ShowSearch
