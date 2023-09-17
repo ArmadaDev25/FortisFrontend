da38fbcfc07841f6c13686401b19d66477dd943a
chauquach1 <chau268@gmail.com>
2023-09-16 17:55:22 -0700

    installed bootstrap for react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,6 @@ function App() {
   console.log('online') // Console log 
   return (
     <div className="App">
-      <h1>Online</h1>
-      
       
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ReactDOM from "react-dom"
+import { Button } from 'react-bootstrap';
+
+
 
 // Router Import
 import { BrowserRouter as Router } from "react-router-dom"
@@ -15,7 +17,4 @@ root.render(
     </Router>
   </React.StrictMode>
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
