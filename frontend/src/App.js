22e5c340a792044bd2313ba3e38835aaa8843581
chauquach1 <chau268@gmail.com>
2023-09-18 14:01:26 -0700

    added navbar component and homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-
+import Navbar from "./components/Navbar";
+import './styles.css'
 
 function App() {
   console.log('online') // Console log 
   return (
-    <div className="App">
-      
+    <div className="d-flex flex-column align-items-center container-fluid bg-success vh-100 p-0">
+      <Navbar/>
     </div>
   );
 }
