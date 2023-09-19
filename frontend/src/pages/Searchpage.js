2c248857a63d8e72ac02a9f88b1dfd828e3a21ed
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2023-09-18 20:28:22 -0700

    Added the Navbar and ShowSearch into the Searchpage file

--- frontend/src/pages/Searchpage.js
+++ frontend/src/pages/Searchpage.js
@@ -2,6 +2,11 @@ import Navbar from "../components/Navbar"
 import ShowSearch from "../components/SearchShow"
 
 const Searchpage = () => {
-
-
+    return (
+        <div>
+            <Navbar />
+            <ShowSearch />
+        </div>
+    )
 }
+export default Searchpage
