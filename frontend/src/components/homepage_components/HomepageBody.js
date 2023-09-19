d4008b888d1e1405601dc824ebe7437012c6a59e
chauquach1 <chau268@gmail.com>
2023-09-19 14:03:44 -0700

    PokemonCardModal rough draft complete

--- frontend/src/components/homepage_components/HomepageBody.js
+++ frontend/src/components/homepage_components/HomepageBody.js
@@ -1,7 +1,11 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Button, Row, Col } from "react-bootstrap";
 import CardStack from "./CardStack";
+import PokemonCardModal from "../complete-modals/PokemonCardModal";
+import { React, useState } from "react";
 
 function HomepageBody() {
+  const [modalShow, setModalShow] = useState(false);
+
   return (
     <Container id="homepage-body" className="text-center bg-secondary vh-100 d-flex flex-column align-items-center p-0">
       <CardStack />
@@ -10,6 +14,17 @@ function HomepageBody() {
       </div>
       <div id="cards-carousel-container">
         <h1>Cards Carousel</h1>
+      </div>
+      <div>
+
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
+
+      <PokemonCardModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
       </div>
     </Container>
   );
