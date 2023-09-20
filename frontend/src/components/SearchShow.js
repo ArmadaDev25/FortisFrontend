d6b8c41eb37fab09949663454be0d8b58270de0c
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2023-09-19 21:45:41 -0700

    Implemented useState into SearchShow

--- frontend/src/components/SearchShow.js
+++ frontend/src/components/SearchShow.js
@@ -3,8 +3,16 @@
 
 // Imports
 import CardSm from "./CardSm";
+// State import
+import {useState, useEffect} from 'react'
+
+
 const ShowSearch = () => {
     const url = `https://api.tcgdex.net/v2/en/sets`
+    
+    // useState variables
+    const [cardEl, setCardEl] = useState(null)
+
     // fetchs the set data
     const getSets = async () =>{
         const response = await fetch(url)
@@ -13,16 +21,22 @@ const ShowSearch = () => {
         console.log(`This is the Data`)
         console.log(data)
         console.log(data[0].name)
-        
-        
+        setCardEl(data) 
     }
-    getSets()
-    return(
-        
-        <p>results</p>
 
+    useEffect(() => {
+        getSets()
+    }, [])
+    console.log('CardEl Value')
+    console.log(cardEl)
+
+    return  (
+        <div>
+            <CardSm cards={cardEl} />
+            
+            
+        </div>  
     )
     
-
 }
 export default ShowSearch
