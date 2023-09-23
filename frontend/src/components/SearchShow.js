23b7323d36ce5544fa2a3232369031678f38e81a
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2023-09-23 16:25:12 -0700

    Work on showing the cards when clicking on a set

--- frontend/src/components/SearchShow.js
+++ frontend/src/components/SearchShow.js
@@ -12,39 +12,20 @@ const ShowSearch = () => {
     console.log(params.input)
     const setsurl = `https://api.tcgdex.net/v2/en/sets/`
     const cardurl = `https://api.tcgdex.net/v2/en/sets/` + params.input
-    // Refresh page Function
-
-    
-
-
-   
-    
-    
-
-   
-
-    
-    
-    
-    //console.log(url)
-    
+    // Refresh page Function   
+    //console.log(url)   
     // useState variables
     const [cardEl, setCardEl] = useState(null)// contains the information pulled from the Pokemon API 
-    const [displayState, setDisplay] = useState(null) // Contains information on what is being displayed 1 means the sets are displayed, 2 means the cars in the set are displayed
-    
-
+    const [displayState, setDisplay] = useState(false) // Contains information on what is being displayed false means the sets are displayed, true means the cars in the set are displayed
     // fetchs the set data
     const getSets = async () =>{
-        
         const response = await fetch(setsurl)
         const data = await response.json()
         // Console Log to make sure the data from the API is coming through
         console.log(`This is the Data`)
         console.log(data)
         //console.log(data[0].name)
-        setCardEl(data)
-        setDisplay(1)
-         
+        setCardEl(data)  
     }
     const getCards = async () => {
         const response = await fetch(cardurl)
@@ -55,62 +36,64 @@ const ShowSearch = () => {
         //console.log(data[0].name)
         setCardEl(data.cards)
     }
-    
-
     // Setting State
     useEffect(() => {
-
-        params.input ? getCards() : getSets()
-        
-        
+        getSets()
+        //params.input ? getCards() : getSets()  
     }, [])
     console.log('CardEl Value')
     console.log(cardEl)
-
     // Logic that loads when data is avalible
     const Loaded = () => {
         console.log(useState)
         console.log(displayState)
-
-
-        const displayArray = cardEl.map((ele, index) =>{
-            return (
-                
-                <Link to={`/search/set/${ele.id}` }>
-
-                    <CardSm
-                        {...ele}
-                        key={index}
-                        
-                    />
+        // Display Sets Function 
+        const displaySets = () => {
+            const displaySetArray = cardEl.map((ele, index) => {
+                return(
+                    <button onClick={getCards}>
+                        <Link to={`/search/set/${ele.id}`}>
+                            <CardSm
+                                {...ele}
+                                key={index}
+                            />
+                        </Link>
+                    </button>
                     
-                </Link>
-                
-                
-                
-
+                )
+            })
+            return(
+                <div>
+                    {displaySetArray}
+                </div>
             )
-        })
-        
-        return(
-            <div>
-                {displayArray}
-            </div>
-        )
-        
-        
-     
-
+        }
+        // Display Cards Function
+        const displayCards = () => {
+            const displayCardArray = cardEl.map((ele, index) =>{
+                return (
+                    <Link to={`/search/set/${ele.id}` }>
+                        <CardSm
+                            {...ele}
+                            key={index}
+                        />
+                        
+                    </Link>
+                )
+            })
+            return(
+                <div>
+                    {displayCardArray}
+                </div>
+            )
+        } 
+        return displayState ? displaySets() : displayCards()
     }
     const Loading = () => {
         return(
             <h1>Fetching Data</h1>
         )
-
     }
-    
-    //
     return cardEl ? Loaded() : Loading()
-    
 }
 export default ShowSearch
