0f6151ca8069557fbedc4903c9136b092560ddca
chauquach1 <chau268@gmail.com>
2023-09-25 00:18:20 -0700

    updated and created components for pokemone-card-modal

--- frontend/src/components/modal-components/CardDisplay.js
+++ frontend/src/components/modal-components/CardDisplay.js
@@ -1,64 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { Container, Row, Col, OverlayTrigger, Image } from 'react-bootstrap';
 
 
 const CardDisplay = (props) => {
-  const [cardData, setCardData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  
-  const getCard = async () => {
-    const tcgdex = "https://api.tcgdex.net/v2/en";
-    
-    try {
-      const response = await fetch(`${tcgdex}/sets/${cardData.set.id}/${cardData.localId}`);
-      
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
+return (
+    <Container id="card-pack-details" className="d-flex flex-column p-0 justify-content-center align-items-center">
+        <Row className="justify-content-center p-0 mb-2">
+          <OverlayTrigger
+            placement="bottom"
+            delay={{ show: 100, hide: 100 }}
+            overlay={props.overlay}
+          >
+          <img
+            className="p-0"
+            id="pkm-card-img"
+            src={`${props.image}/high.png`}
+            alt="Pokemon Card"
+            style={{ maxHeight: "360px", width: "auto", height: "auto", display: "block", margin: "0 auto" }}
+          />
+          </OverlayTrigger>
+        </Row>
+        <Col id="pkm-card-details" className="d-none d-sm-flex flex-column justify-content-center text-center border rounded py-2 w-75">
+          <Row fluid className="px-0">
+            <p id="pkm-variant" className="pkm-card-detail text-capitalize">Variant: {Object.entries(props.variants)
+              .filter(([variant, value]) => value === true)
+              .map(([variant]) => variant)
+              .join(', ')}
+            </p>
+          </Row>
 
-      const data = await response.json();
-      
-      setCardData(data);
-      setLoading(false); // Set loading to false once data is fetched
-    } catch (error) {
-      setError(error);
-      setLoading(false); // Set loading to false on error
-    }
-  }
+          <Row className="d-inline-flex flex-row justify-content-center px-0">
+              <p id="card-count" className="pkm-card-detail">Card: {props.localId}/{props.cardcountofficial}</p>
+              <p id="card-category" className="pkm-card-detail">Category: {props.category}</p>
+          </Row>
 
-  useEffect(() => {
-    
-    getCard();
-  }, []);
-
-  // Render the loaded data
-  const loaded = () => {
-    return (
-      <div id="card-pack-details" className="d-flex flex-column mx-2 align-content-center" style={{ maxWidth: "250px" }}>
-        <img src={`${cardData.image}/high.png`} alt="Pokemon Card" />
-        
-      </div>
-    )
-  }
-
-  // Render loading state
-  const renderLoading = () => {
-    return <h1>Loading...</h1>;
-  }
-
-  // Render error state
-  const renderError = () => {
-    return <p>Error: {error.message}</p>;
-  }
-
-  // Conditional rendering based on loading and error states
-  if (loading) {
-    return renderLoading();
-  } else if (error) {
-    return renderError();
-  } else {
-    return loaded();
-  }
+          <Row className="">
+            <p id="illustrator" className="pkm-card-detail px-0">Illustrator: {props.illustrator}</p>
+          </Row>
+          <Row className="justify-content-center">
+            <Image
+              id="series-logo" 
+              src={`${props.setlogo+ '.png'}`} 
+              alt="Pokemon Card" />
+          </Row>
+        </Col>
+    </Container>
+  )
 }
 
 export default CardDisplay;
