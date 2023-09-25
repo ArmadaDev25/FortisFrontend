a64bc1a72bad7a8692cd9df08a66ee83407c421d
chauquach1 <chau268@gmail.com>
2023-09-25 00:18:20 -0700

    updated and created components for pokemone-card-modal

--- frontend/src/components/complete-modals/PokemonCardModal.js
+++ frontend/src/components/complete-modals/PokemonCardModal.js
@@ -1,21 +1,22 @@
 import React from "react";
-import { Modal, closeButton, Row, Col, Container, Image, Overlay, Tooltip, OverlayTrigger, ModalHeader } from "react-bootstrap";
+import { Modal, Row, Col, Container, Image, Tooltip, OverlayTrigger, ModalHeader } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import GetCardType from "../modal-components/GetCardType";
+
 
 function PokemonCardModal (props) {
   const [series, setSeries] = useState(null)
   const [cardData, setCardData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  // const [show, setShow] = useState(false);
-  // const handleClose = () => setShow(false);
-  
+
+
   
   const getCard = async () => {
     const tcgdexurl = "https://api.tcgdex.net/v2/en";
     
     try { 
-      const card = await fetch(`${tcgdexurl}/sets/swsh1/142`);
+      const card = await fetch(`${tcgdexurl}/sets/swsh4/185`);
       // const response = await fetch(`${tcgdex}/sets/${cardData.set.id}/${cardData.localId}`);
       
       if (!card.ok) {
@@ -25,7 +26,7 @@ function PokemonCardModal (props) {
       const data = await card.json();
       
 
-      console.log(data);
+      // console.log(data);
       // console.log(data.set.logo);
       setCardData(data);
       setLoading(false); // Set loading to false once data is fetched
@@ -40,44 +41,9 @@ function PokemonCardModal (props) {
     getCard();
   }, []);
 
-  const renderTooltip = (props) => (
-    <Tooltip id="button-tooltip" className="d-sm-none" {...props}>
-      <Col fluid className="d-flex flex-column p-2">
-        <Container id="pkm-card-details" className="border rounded">
-          <Row className="px-2">
-          <p id="pkm-card-detail" className="text-capitalize">Variant: {Object.entries(cardData.variants)
-            .filter(([variant, value]) => value === true)
-            .map(([variant]) => variant)
-            .join(', ')}
-          </p>
-          </Row>
-          <Row className="px-0">
-            <Col className="px-0">
-              <p id="pkm-card-detail" className="px-2">Card: {cardData.localId}/{cardData.set.cardCount.official}</p>
-            </Col>
-            <Col className="px-0">
-              <p id="pkm-card-detail" className="px-2">Category: {cardData.category}</p>
-            </Col>
-            </Row>
-
-            <Row className="px-2">
-              <p id="pkm-card-detail" className="px-0">Illustrator: {cardData.illustrator}</p>
-            </Row>
-          <Row>
-            <Image
-              id="series-logo" 
-              src={`${cardData.set.logo + '.png'}`} 
-              alt="Pokemon Card" 
-            />
-          </Row>
-        </Container>
-      </Col>
-    </Tooltip>
-  );
-
   // Render the loaded data
   const loaded = () => {
-    
+    console.log(cardData);
 
     return (
       <Modal
@@ -86,74 +52,29 @@ function PokemonCardModal (props) {
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
-        <ModalHeader closeButton>
-        </ModalHeader>
-        <Modal.Body className="d-flex flex-column flex-sm-col flex-lg-row justify-content-evenly px-2 shadow">
+        <ModalHeader closeButton></ModalHeader>
+        <Modal.Body className="d-flex flex-column flex-sm-row justify-content-around px-2 shadow">
 
-        <div id="card-pack-details" className="d-flex flex-column flex-lg-row mx-2 justify-content-center align-items-center">
-          <Row className="d-flex flex-row justify-content-center">
-            <Col className="d-flex flex-column p-0 mb-2 justify-content-center align-items-center ">
-              <OverlayTrigger
-                placement="bottom"
-                delay={{ show: 100, hide: 100 }}
-                overlay={renderTooltip}
-              >
-              <img
-                id="pkm-card-img"
-                src={`${cardData.image}/high.png`}
-                alt="Pokemon Card"
-                style={{ maxHeight: "360px", width:"250px", height: "auto" }} // Image scaling styles
-              />
-              </OverlayTrigger>
-            </Col>
-            <Col fluid md={9} className="d-none d-sm-flex flex-column text-center p-0">
-              <Container id="pkm-card-details" className="border rounded" style={{maxWidth:"300px"}}>
+          {/* Gets Card Category then Returns Appropriate Modal  */}
+          {GetCardType(cardData)}
 
-                <Row className="px-0">
-                  <p id="pkm-card-detail" className="text-capitalize px-2">Variant: {Object.entries(cardData.variants)
-                    .filter(([variant, value]) => value === true)
-                    .map(([variant]) => variant)
-                    .join(', ')}
-                  </p>
-                </Row>
-
-                <Row className="d-flex flex-row justify-content-around px-0">
-                  <Col className="px-0">
-                    <p id="pkm-card-detail" className="px-2">Card: {cardData.localId}/{cardData.set.cardCount.official}</p>
-                  </Col>
-                  <Col className="px-0">
-                    <p id="pkm-card-detail" className="px-2">Category: {cardData.category}</p>
-                  </Col>
-                </Row>
-
-                <Row className="px-2">
-                  <p id="pkm-card-detail" className="px-0">Illustrator: {cardData.illustrator}</p>
-                </Row>
-                <Row>
-                  <Image
-                    id="series-logo" 
-                    src={`${cardData.set.logo + '.png'}`} 
-                    alt="Pokemon Card" />
-                </Row>
-              </Container>
-            </Col>
-          </Row>
-        </div>
-
-        <Container id="card-action-details" className="d-flex flex-column bg-dark-subtle rounded shadow p-0 d-sm-block align-self-start text-center" >
+        <Container id="card-action-details" className="d-flex flex-column bg-dark-subtle rounded shadow p-0 d-sm-block align-self-start text-center">
           <Container id="pokemon-details" className="d-flex flex-row flex-wrap w-100 p-2">
             <Col className="d-flex flex-row justify-content-between align-items-center">
               <Col xs={10}>
-                <p className="m-0 text-white rounded-top text-start fs-2">{cardData.name}</p>
+                <p className="m-0 text-white rounded-top text-start pkm-name">{cardData.name}</p>
               </Col>
               <Col xs={2}>
                 <p className="my-0 p-0"><span style={{fontSize:"12px"}}>HP</span>{cardData.hp}</p>
               </Col>
             </Col>
-            <Container className="text-start" style={{fontSize: "15px"}}>
+            <Container className="text-start border-top mt-1 pt-1" style={{fontSize: "15px"}}>
               <Row xs={12}>
                 <Col className="p-0"><p className="m-0 p-0">Stage: {cardData.stage}</p></Col>
                 <Col className="p-0"><p className="my-0 p-0">Types: {cardData.types}</p></Col>
+                <Image
+                  // src={}
+                />
               </Row>
               <Row xs={12}>
                 <Col className="p-0"><p className="m-0 p-0">Evolves From: {cardData.evolveFrom}</p></Col>
@@ -177,19 +98,23 @@ function PokemonCardModal (props) {
                 <div id="pokemon-attack-name" className="d-flex flex-row">
                   <h4 className="m-0 text-right">{attack.name}</h4>
                 </div>
-                <div id="pokemon-attacks-details" className="d-flex flex-column text-center">
+                <Container id="pokemon-attacks-details" className="d-flex flex-column text-center">
                   <div className="d-flex flex-row">
                     {attack.effect ? <p className="m-0">{attack.effect}</p> : ""}
                   </div>
-                  <div className="d-flex flex-row justify-content-evenly text-wrap m-0">
+                  <Container className="d-flex flex-row justify-content-evenly text-wrap m-0">
                     <Col>
                       {attack.cost ? <p className="mb-0">Cost: {attack.cost} </p>: ""}
+                      {/* {attack.cost ? <p className="mb-0">Cost: {showCost()} </p>: ""} */}
+                      {/* {attack.cost ? <p className="mb-0">Cost:
+                        <img id="energy-symbol" src={Colorless} /> 
+                      </p>: ""} */}
                     </Col>
                     <Col>
                       {attack.damage ? <p className="mb-0">Dmg: {attack.damage} </p>: ""}
                     </Col>
-                  </div>
-                </div>
+                  </Container>
+                </Container>
               </div>
             ))
           : null}
