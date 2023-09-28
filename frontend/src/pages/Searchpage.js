e47304d00e4453b2258b79fce558d277648c1241
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2023-09-27 23:02:44 -0700

    Recommit after fixing merge error

--- frontend/src/pages/Searchpage.js
+++ frontend/src/pages/Searchpage.js
@@ -1,12 +1,14 @@
 import { Container } from 'react-bootstrap'
 import {useState} from 'react'
 import ShowSearch from "../components/SearchShow"
-
+import CollectionsSB from '../components/collections_comps/CollectionComp_SB'
 const Searchpage = () => {
     
     
     return (
         <Container fluid className='d-flex flex-column justify-content-center align-items-center w-100'>
+            
+            <CollectionsSB />
             <h1>Card Database</h1>
             <Container fluid className='w-100'>
                 <ShowSearch />
