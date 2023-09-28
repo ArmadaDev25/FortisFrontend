d6d3aed657d767bd5064542b5e95dab44282e602
Joberman055 <jacksonoberman@gmail.com>
2023-09-27 22:40:37 -0700

    move backend connection to app.js

--- frontend/src/pages/Collections.js
+++ frontend/src/pages/Collections.js
@@ -1,55 +1,6 @@
 import { useState, useEffect } from "react"
 
 const Collections = () => {
-    const [collections, setCollections] = useState(null)
-
-    const collectionURL = "https://fortis-backend-c8c49038070a.herokuapp.com/collections/"
-
-    //Collection crud
-    const getCollections = async () => {
-        const response = await fetch(collectionURL)
-        const data = await response.json()
-        setCollections(data.data)
-    }
-
-    const createCollection = async (collection) => {
-        await fetch(collectionURL, {
-            method:"post",
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(collection)
-        })
-
-        getCollections()
-    }
-
-    const updateCollection = async (collection, id) => {
-        await fetch(collectionURL + id, {
-            method:"put",
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(collection)
-        })
-
-        getCollections()
-    }
-
-    const deleteCollection = async (id) => {
-        await fetch(collectionURL + id, {
-            method: "delete"
-        })
-
-        getCollections()
-    }
-
-    useEffect(() => {
-        getCollections()
-    }, [])
-
-
-
     return (
         <>
             <h1>Card Collections</h1>
