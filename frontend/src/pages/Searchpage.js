daf8f79f58e1947cdea40db743b519d395b59ed8
Joberman055 <jacksonoberman@gmail.com>
2023-09-28 05:09:48 -0700

    rebase

--- frontend/src/pages/Searchpage.js
+++ frontend/src/pages/Searchpage.js
@@ -6,14 +6,13 @@ const Searchpage = () => {
     
     
     return (
-        <Container fluid className='d-flex flex-column justify-content-center align-items-center w-100'>
-            
+        <div>
+        
             <CollectionsSB />
+            
             <h1>Card Database</h1>
-            <Container fluid className='w-100'>
-                <ShowSearch />
-            </Container>
-        </Container>
+            <ShowSearch />
+        </div>
     )
 }
 export default Searchpage;
