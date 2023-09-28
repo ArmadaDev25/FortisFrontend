bcc06fdcd8cd8c9aafdf226d35fa38274a7116bc
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2023-09-28 12:00:46 -0700

    Work on CollectionInfo

--- frontend/src/pages/CollectionInfo.js
+++ frontend/src/pages/CollectionInfo.js
@@ -1,8 +1,15 @@
 // Shows info and cards on the selected collection
 import CollectionDisplayInfo from "../components/collections_comps/CollectionDisplayInfo"
 import CollectionDisplayCards from "../components/collections_comps/CollectionDisplayCards"
-
-const CollectionInfo = () => {
+import {useParams} from "react-router-dom"
+const CollectionInfo = (props) => {
+    console.log(props)
+    const params = useParams()
+    
+    return(
+        <h1></h1>
+        
+    )
 
 
 }
