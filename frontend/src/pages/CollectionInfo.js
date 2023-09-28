5fbf78ab285a9585f2e7d2ae32721774a480edbd
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2023-09-28 12:49:45 -0700

    Collectioninfo now gets the proper url to call through the params

--- frontend/src/pages/CollectionInfo.js
+++ frontend/src/pages/CollectionInfo.js
@@ -2,14 +2,19 @@
 import CollectionDisplayInfo from "../components/collections_comps/CollectionDisplayInfo"
 import CollectionDisplayCards from "../components/collections_comps/CollectionDisplayCards"
 import {useParams} from "react-router-dom"
+import {useState, useEffect} from "react"
 
-
-const CollectionInfo = (props) => {
-    console.log(props)
-    const params = useParams()
+const CollectionInfo = () => {
     
+    const params = useParams()
+    const [collection, setCollection] = useState(null)
+
+    const url = `https://fortis-backend-c8c49038070a.herokuapp.com/collections/${params.id}`
+    console.log(url)
     return(
-        <h1></h1>
+        <h1>
+
+        </h1>
         
     )
 
