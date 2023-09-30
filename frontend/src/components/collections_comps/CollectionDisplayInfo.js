1993aa6bfacc00d8fc33755ccc9714d18b77b9bc
chauquach1 <chau268@gmail.com>
2023-09-29 19:33:14 -0700

    collection pages have user chosen icons

--- frontend/src/components/collections_comps/CollectionDisplayInfo.js
+++ frontend/src/components/collections_comps/CollectionDisplayInfo.js
@@ -2,7 +2,7 @@
 import CollectionDisplayCards from "./CollectionDisplayCards"
 import CollectionCardModal from "../complete-modals/CollectionCardModal"
 import {useState, useEffect} from "react"
-import { Container } from "react-bootstrap"
+import { Container,Image } from "react-bootstrap"
 
 const CollectionDisplayInfo = ({col}) => {
     const [toDisplay, setDisplay] = useState(null)
@@ -26,12 +26,25 @@ const CollectionDisplayInfo = ({col}) => {
     }
     const loaded = () => {
         return(
-            <Container className="text-center">
-            <Container className="d-flex-flex-column">
-                <h1 className="m-0">{toDisplay.name}</h1>
-                <p className="m-0 fs-6 text-secondary">{toDisplay.description}</p>
-            </Container>
-            <CollectionDisplayCards cards={toDisplay.cards}/>
+            <Container className="text-center p-1">
+                <Container fluid className="d-flex align-items-center flex-column">
+                    <Container fluid className="d-flex justify-content-center align-items-center h-100 flex-row">
+                        <Image
+                            src={toDisplay.img}
+                            className=""
+                            style={{width:"25px", height:"auto"}} 
+                        />
+                        {/* <h1 className="mx-2 my-0">{toDisplay.name}</h1> */}
+                        <h1 className="mx-2 my-0">{toDisplay.name}</h1>
+                        <Image
+                            src={toDisplay.img}
+                            className=""
+                            style={{width:"25px", height:"auto"}} 
+                        />
+                    </Container>
+                        <p className="m-0 fs-6 text-secondary">{toDisplay.description}</p>
+                </Container>
+                <CollectionDisplayCards cards={toDisplay.cards}/>
             </Container>
             
         )
